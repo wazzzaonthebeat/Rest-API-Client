1c75abf79c24d45c50ebd2bc4900d7ab44251615
Moemedi Rakhudu <rakhudu@tablemate.net>
2022-09-16 15:05:16 -0400

    Add project files.

---
+++ Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace RESTAPI.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RESTAPI.Helpers;
+
+namespace RESTAPI;
+
+public sealed partial class MainWindow : WindowEx
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
+    }
+}
